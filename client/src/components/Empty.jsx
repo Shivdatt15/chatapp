e18e9ebd8af091652b4cc089172f0c7b8c25ca75
shivdatt15 <shivdattlakhe999@gmail.com>
2025-06-16 13:29:55 +0530

    AI Avtar added

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -6,6 +6,7 @@ function Empty() {
   return (
    <div className="border-conversation-border border-1 w-full bg-panel-header-background flex flex-col h-[100vh] border-b-4 border-b-blue-500 items-center justify-center">
           <Image src="/chatapp.gif" alt="chatapp" height={300} width={300}/>
+          <h1 className="mt-4 text-2xl font-bold text-white">Welcome to ChatApp...</h1>
   </div>
   );
 }
